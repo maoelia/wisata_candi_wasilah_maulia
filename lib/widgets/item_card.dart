490a7eae0b5eeb35677434d8759cc4a656fd3680
Wasilah Maulia <wasilahmaulia_2327240119@mhs.mdp.ac.id>
2025-11-23 22:57:31 +0700

    Grid View

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -6,9 +6,7 @@ class ItemCard extends StatelessWidget {
   //TODO: 1. Deklarasikan variabel yang dibutuhkan dan pasang pada konstruktor
   final Candi candi;
 
-  const ItemCard({super.key,
-  required this.candi,
-  });
+  const ItemCard({super.key, required this.candi,});
 
   @override
   Widget build(BuildContext context) {
@@ -24,14 +22,46 @@ class ItemCard extends StatelessWidget {
       child: Card(
         // TODO: 2. Tetapkan parameter shape, margin, dan elevation dari Cari
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10)
+          borderRadius: BorderRadius.circular(10),
         ),
-      )
-          //TODO: 3. Buat Image sebagai anak dari Colum
-          //TODO: 4. Buat Text sebagai anak dari column
-          //TODO: 5. Buat Text sebagai anak dari column
-        ],
-      ),
+        margin: EdgeInsets.all(4),
+        elevation: 1,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //TODO: 3. Buat Image sebagai anak dari Column
+            Expanded(
+              //TODO: 7. Implementasi hero animation
+              child: ClipRRect(
+                borderRadius: BorderRadiusGeometry.circular(15),
+                child: Image.asset(candi.imageAsset,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            //TODO: 4. Buat Text sebagai anak dari column
+            Padding(
+                  padding: EdgeInsets.only(left: 16, top: 8),
+                  child: Text(candi.name,
+                    style: TextStyle(
+                      fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                  ),
+                ),
+            //TODO: 5. Buat Text sebagai anak dari column
+            Padding(
+              padding: EdgeInsets.only(left: 16, top: 8),
+              child: Text(candi.type,
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+          ],
+        ),
+       ),
     );
   }
 }
